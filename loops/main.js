4b9b2e57df02a1a94c22d952d1265677e99de825
Hilton Adolinama <hiltonadolinama@Hiltons-MacBook-Pro.local>
2018-01-21 19:39:38 -0700

    Loops

---
+++ loops/main.js
@@ -0,0 +1,20 @@
+console.log("I am connected to the Console!");
+
+var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+};
+
+for (var j = 11; j < 20; j++) {
+    console.log(j);
+}
+
+
+var names = ["John", "Mary", "Kim", "Potter", "VoldeMorte","Bright","Magaret"]
+
+var k = 0; //need the conditon b4 start of loop
+while (k < names.length) {
+    console.log(names[k]);
+    k++; //need counter after loop
+}
